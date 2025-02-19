04135dd1cda1a6a16d21e4e4b9bed9bfa38e29dd
1ou1s123 <yunenqian@gmail.com>
2025-02-19 11:56:38 +0800

    week01

---
+++ week01-1.cpp
@@ -0,0 +1,16 @@
+/// week01-1.cpp 瘋狂程設 兩數相加
+/// File-New empty 開新的檔案
+/// Ctrl-s 存檔成 week01-1.cpp 要記得加 .cpp
+///
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+	scanf( "%d" , &a );
+	scanf( "%d" , &b );
+
+	printf( "%d" , a+b );
+
+}
+
